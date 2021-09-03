3c403a8bf5772666b28e7764b42e00a620434d18
Christian Osada <christian.osada@lntinfotech.com>
2021-09-03 11:18:52 -0400

    shop changes

--- src/app/pipes/datePipe/date-pipe.pipe.ts
+++ src/app/pipes/datePipe/date-pipe.pipe.ts
@@ -7,10 +7,9 @@ export class DatePipePipe implements PipeTransform {
   months: string[] = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
   transform(value: Date): string {
     const splitDate: string[] = value.toString().split('-');
-    const month: string = this.months[parseInt(splitDate[1])];
+    const month: string = this.months[parseInt(splitDate[1]) - 1];
     const day: string = splitDate[2];
     const year: string = splitDate[0];
     return `${month} ${day}, ${year}`;
   }
-
 }
