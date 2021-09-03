9779aa1fa52f44655511b85606dd5a2346c4f975
unknown <Lntinfotech@INFEDL31167.lntinfotech.com>
2021-09-03 11:30:02 -0400

    added the component for the employee to view book requests.

---
+++ src/app/components/employee-view-requests/employee-view-requests.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { EmployeeViewRequestsComponent } from './employee-view-requests.component';
+
+describe('EmployeeViewRequestsComponent', () => {
+  let component: EmployeeViewRequestsComponent;
+  let fixture: ComponentFixture<EmployeeViewRequestsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ EmployeeViewRequestsComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(EmployeeViewRequestsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/components/employee-view-requests/employee-view-requests.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-employee-view-requests',
+  templateUrl: './employee-view-requests.component.html',
+  styleUrls: ['./employee-view-requests.component.css']
+})
+export class EmployeeViewRequestsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
