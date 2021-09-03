78ea3069d1bfd904a1a2318b6a9a16aa2cf33c9e
unknown <Lntinfotech@INFEDL31167.lntinfotech.com>
2021-09-03 16:09:25 -0400

    commiting all the changes.

--- src/app/components/employee-view-requests/employee-view-requests.component.ts
+++ src/app/components/employee-view-requests/employee-view-requests.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { RequestInfoServiceService } from 'src/app/services/requestInfo/request-info-service.service';
 import { BookRequest } from 'src/app/models/bookRequest';
 import { SearchApiService } from 'src/app/services/searchApi/search-api.service';
+import { Book } from 'src/app/models/book';
+import { Work } from 'src/app/models/work';
+import { AddToShopService } from 'src/app/services/addToShop/add-to-shop.service';
+
 
 @Component({
   selector: 'app-employee-view-requests',
@@ -10,19 +14,23 @@ import { SearchApiService } from 'src/app/services/searchApi/search-api.service'
 })
 export class EmployeeViewRequestsComponent implements OnInit {
 
-  constructor(private requestService: RequestInfoServiceService, private searchApiService: SearchApiService) { }
+  constructor(private requestService: RequestInfoServiceService, private searchApiService: SearchApiService, private addToShopService: AddToShopService) { }
 
   ngOnInit(): void {
     this.getRequest();
   }
 
   requests?: BookRequest[];
+  books?: Work[];
 
   getRequest() {
     this.requestService.getAllRequestList().subscribe(
+      
       response => {
+        response.forEach(item => {
+          item.dataTarget = '#' + item.id;
+        });
         this.requests = response;
-        console.log(response);
       } 
     )
   }
@@ -35,9 +43,24 @@ export class EmployeeViewRequestsComponent implements OnInit {
 
   getBooks(author: string, title: string) {
     this.searchApiService.searchByAuthorAndTitle(author, title).subscribe(
-      response => console.log(response)
+      response => {
+        this.books = response.docs;
+        console.log(response);
+      }
     )
   }
 
+  addToShop(item: Work, acceptedRequest: number) {
+    
+    const priceInput = Number(prompt('How much should this book cost?'));
+    const inventoryInput = Number(prompt('How many of this book do you have?'));
+
+    const completeItem = { ...item, price: priceInput, inventory: inventoryInput };
+    this.requestService.approveRequest(completeItem, acceptedRequest).subscribe(
+      response => {
+        alert(`Successfully added ${item.title} by ${item.author_name} to the shop!`);
+      }
+    )
+  }
 
 }
--- src/app/models/bookRequest.ts
+++ src/app/models/bookRequest.ts
@@ -1,6 +1,8 @@
 import { User } from "./user";
 
 export interface BookRequest {
+    id: number;
+    dataTarget: string;
     requester: User;
     title: string;
     author: string;
